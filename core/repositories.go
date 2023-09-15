b43ffeeff1fc5b13aade1e838ae1c4b99abeb61e
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-09-14 21:16:05 -0300

    chore: repository interfaces

---
+++ core/repositories.go
@@ -0,0 +1,12 @@
+package core
+
+type AuthRepository interface {
+	SignIn(credentials Credentials) (User, error)
+	SignUp(credentials Credentials, user User) (User, error)
+}
+
+type SessionRepository interface {
+	CreateSession(user User) (Session, error)
+	GetSession(sessionID Uuid) (Session, error)
+	SignOut(sessionID Uuid) error
+}
