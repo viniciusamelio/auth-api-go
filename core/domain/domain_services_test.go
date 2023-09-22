b16087718425fea8c4a83ffb1c2fae4588f9c789
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-09-22 15:33:23 -0300

    test✅: ensuring all test-missing methods are working as expected

--- core/domain/domain_services_test.go
+++ core/domain/domain_services_test.go
@@ -96,3 +96,79 @@ func TestSUTShouldReturnSessionWhenSignInIsOk(t *testing.T) {
 	assert.Equal(t, session.Active, true)
 	assert.Nil(t, err)
 }
+
+func TestSUTShouldReturnErrorWhenSignOutFails(t *testing.T) {
+	authRepo := new(mockAuthRepository)
+	sessionRepo := new(mockSessionRepository)
+	sessionRepo.On("SignOut", mock.Anything).Return(
+		&core.DefaultError{
+			Message: "Session could not be signed out",
+		},
+	)
+	sut := AuthenticationService{
+		sessionRepository: sessionRepo,
+		authRepository:    authRepo,
+	}
+
+	error := sut.Logout(Session{
+		Id: faker.UUIDHyphenated(),
+	})
+
+	assert.NotNil(t, error)
+	assert.Equal(t, "Session could not be signed out", error.Error())
+}
+
+func TestSUTShouldReturnErrorWhenAuthRepoSignUpFails(t *testing.T) {
+	authRepo := new(mockAuthRepository)
+	sessionRepo := new(mockSessionRepository)
+	authRepo.On("SignUp", mock.Anything, mock.Anything).Return(
+		User{}, &core.DefaultError{
+			Message: "User could not be created",
+		},
+	)
+	sut := AuthenticationService{
+		sessionRepository: sessionRepo,
+		authRepository:    authRepo,
+	}
+
+	_, error := sut.SignUp(Credentials{
+		Username: "test",
+		Password: "test",
+	}, User{
+		Email: faker.Email(),
+		Name:  faker.Name(),
+		Id:    faker.UUIDHyphenated(),
+	})
+
+	assert.NotNil(t, error)
+	assert.Equal(t, "User could not be created", error.Error())
+}
+func TestSUTShouldReturnUserWhenAuthRepoSignSucceeds(t *testing.T) {
+	authRepo := new(mockAuthRepository)
+	sessionRepo := new(mockSessionRepository)
+	authRepo.On("SignUp", mock.Anything, mock.Anything).Return(
+		User{
+			Email: faker.Email(),
+			Name:  faker.Name(),
+			Id:    faker.UUIDHyphenated(),
+		}, nil,
+	)
+	sut := AuthenticationService{
+		sessionRepository: sessionRepo,
+		authRepository:    authRepo,
+	}
+
+	user, _ := sut.SignUp(Credentials{
+		Username: "test",
+		Password: "test",
+	}, User{
+		Email: faker.Email(),
+		Name:  faker.Name(),
+		Id:    faker.UUIDHyphenated(),
+	})
+
+	assert.IsType(t, User{}, user)
+	assert.NotNil(t, user.Email)
+	assert.NotNil(t, user.Name)
+	assert.NotNil(t, user.Id)
+}
