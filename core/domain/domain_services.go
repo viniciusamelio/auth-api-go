0b9933c9a5e5abc184b2c5b09554bdc62f638750
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-09-22 08:36:52 -0300

    refactor🔨: making domain services structs instead of interfaces and adding it back to domain package

--- core/domain/domain_services.go
+++ core/domain/domain_services.go
@@ -1,10 +1,39 @@
 package domain
 
-type AuthenticationService interface {
-	Authenticate(credentials Credentials) (Session, error)
-	Logout(session Session) error
+// Authentication domain service
+type AuthenticationService struct {
+	authRepository    AuthRepository
+	sessionRepository SessionRepository
 }
 
-type SessionService interface {
-	RecoverSession(sessionID string) (Session, error)
+func (this *AuthenticationService) New(AuthRepository AuthRepository, SessionRepository SessionRepository) {
+	this.authRepository = AuthRepository
+	this.sessionRepository = SessionRepository
+}
+
+func (this *AuthenticationService) Authenticate(credentials Credentials) (Session, error) {
+	user, error := this.authRepository.SignIn(credentials)
+	if error != nil {
+		return Session{}, error
+	}
+	session, error := this.sessionRepository.CreateSession(user)
+	return session, error
+
+}
+func (this *AuthenticationService) Logout(session Session) error {
+	error := this.sessionRepository.SignOut(session.Id)
+	if error != nil {
+		return error
+	}
+	return nil
+}
+
+// Session domain service
+type SessionService struct {
+	sessionRepository SessionRepository
+}
+
+func (this *SessionService) RecoverSession(sessionID string) (Session, error) {
+	session, error := this.sessionRepository.GetSession(sessionID)
+	return session, error
 }
